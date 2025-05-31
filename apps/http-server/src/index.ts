19ec2c0c775f57de37d07bd3f8fd5f0cf951e683
Vignesh D <vignesh.d9123@gmail.com>
2025-05-31 19:37:27 +0530

    init http server

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello")
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, ()=>{
+    console.log("HTTP server is running on port", PORT)
+})
